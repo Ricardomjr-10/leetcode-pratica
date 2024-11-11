de4ab5783b7674d0e6fe2c5db37b973e8d93bfdf
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-10 21:19:05 -0300

    funcao criada com ajuda da IA

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-const l1 = [9,9,9,9,9,9,9]
-const l2 = [9,9,9,9]
-
-const somaDois = (l1, l2) => {
-    let numberl1 = ''
-    let numberl2 = ''
-    for (let i = 0; i < l2.length; i++) {
-    numberl1 += l1[l1.length -1 - i]
-    numberl2 += l2[l2.length -1 - i]
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
     }
-    let res = Number(numberl1) + Number(numberl2)
-    return res.toString().split('').reverse().join()
-}
+  }
+  
+  function addTwoNumbers(l1, l2) {
+    let dummy = new Node(0);
+    let current = dummy;
+    let carry = 0;
+  
+    while (l1 || l2) {
+      let x = l1 ? l1.val : 0;
+      let y = l2 ? l2.val : 0;
+      let sum = x + y + carry;
+      carry = Math.floor(sum / 10);
+      current.next = new Node(sum % 10);
+      current = current.next;
+      if (l1) l1 = l1.next;
+      if (l2) l2 = l2.next;
+    }
+  
+    if (carry > 0) {
+      current.next = new Node(carry);
+    }
+  
+    return dummy.next;
+  }
 
-console.log(somaDois(l1, l2))
-console.log(typeof(numberl1= {}))
-console.log(typeof(numberl2 = {a:'a'}), numberl2)
+  const l1 = [2,4,3]
+  const l2 = [5,6,4]    
+
+  console.log(addTwoNumbers(l1,l2))
--- prefixCommom.js
+++ prefixCommom.js
@@ -1,6 +1,16 @@
 
 const includesPrefix = strs => {
+   if(strs.length === 0) {
+    return ""
+   }
+   let prefix = strs[0]
+   for (let i = 0; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+        prefix = prefix.slice(0, -1)
+    }
    
+   }
+   return prefix
 }
 
 const strs = ["flower", "flow", "flight"]
