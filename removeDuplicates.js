8d2c5fe1459281000e590edf109e88898e729326
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-15 16:43:41 -0300

    feat:function pronta

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,7 +1,14 @@
 const removeDuplicates = nums => {
-
+for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] === nums[j]) {  
+            nums.splice(j, 1)
+            j-- 
+        }
+    }
 }
-
+return nums.length
+}
 const nums = [1,1,2]
 
 console.log(removeDuplicates(nums), nums)
