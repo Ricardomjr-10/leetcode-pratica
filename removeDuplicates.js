c48a9bae641fcad8eafa5dcca444f26f1a4f932b
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-15 16:17:52 -0300

    build: function removerDuplicates

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,10 +1,10 @@
 const removeDuplicates = nums => {
-let array = []
-for (let i = 0; i < nums.length; i++) {
-    if (!array.includes(nums[i])) {
-        array.push(nums[i])
-    }
-}
+// let array = []
+// for (let i = 0; i < nums.length; i++) {
+//     if (!array.includes(nums[i])) {
+//         array.push(nums[i])
+//     }
+// }
 }
 
 const nums = [1,1,2]
