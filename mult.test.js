0656b7a5cd5358427e5263a017200b97b62ab993
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-16 14:53:05 -0300

    commit teste

--- mult.test.js
+++ mult.test.js
@@ -1,4 +1,8 @@
-const multi = require('./mult.js')
+//const multi = require('./mult.js')
+
+function multi (a, b) {
+    return a * b
+}
 
 test('multiplies 2 * 2 to equal 4', () => {
     expect (multi(2, 2)).toBe(4)
